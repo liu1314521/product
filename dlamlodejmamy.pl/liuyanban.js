4dfe9f7bb78752b9beddab921638d44a3ec8dbc9
liu1314521 <40618013+liu1314521@users.noreply.github.com>
2020-11-19 13:58:06 +0800

    Create liuyanban.js

---
+++ dlamlodejmamy.pl/liuyanban.js
@@ -0,0 +1,172 @@
+document.write("  <div class=\"product-col container mt60\">");
+document.write("        <div class=\"common-title\">");
+document.write("            <b>Our Products<\/b>");
+document.write("            <h2>Let us see What we can offer<\/h2>");
+document.write("        <\/div>");
+document.write("        <ul class=\"product-list\">");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/sand-washing-machine.html\">");
+document.write("                    <img src=\"\/proimg\/1_20170317043919320b.jpg\" alt=\"Sand Washing Machine\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/sand-washing-machine.html\">Sand Washing Machine<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>Sand washing machine is a kind of high-efficient machine, which is produced by fote according to the actual situations of sand industry around the world.<\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/sand-washing-machine.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/briquette-machine.html\">");
+document.write("                    <img src=\"\/proimg\/1_2017617057543.jpg\" alt=\"Briquette Machine\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/briquette-machine.html\">Briquette Machine<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>Briquette machine is also called briquetting press machine, briquetting machine or pressure ball machine.<\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/briquette-machine.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/ball-mill.html\">");
+document.write("                    <img src=\"\/proimg\/2_20170317043818414b.jpg\" alt=\"Ball Mill\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/ball-mill.html\">Ball Mill<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>Ball mill is the key machine to grind the crushed materials. Ball mill machine is widely used to process cement, silicate product, building material, refractory material, fertilizer, ceramics and glass, etc.<\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/ball-mill.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/sand-making-machine.html\">");
+document.write("                    <img src=\"\/proimg\/1_20170317044015297b.jpg\" alt=\"Sand Making Machine\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/sand-making-machine.html\">Sand Making Machine<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>VSI sand maker, also called VSI crusher, is the major machine for sand making plant. During the crushing process of sand making machine, raw materials hit each other and not hit any metal parts.<\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/sand-making-machine.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/jaw-crusher.html\">");
+document.write("                    <img src=\"\/proimg\/1_20170317044932420b.jpg\" alt=\"Jaw Crusher\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/jaw-crusher.html\">Jaw Crusher<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>Jaw crusher  machine, also named jaw crusher for short, is the earliest crusher. <\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/jaw-crusher.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("            <li class=\"product-item\">");
+document.write("                <a rel=\"nofollow\" href=\"\/products\/mobile-crushers.html\">");
+document.write("                    <img src=\"\/proimg\/1_20170317044326496b.jpg\" alt=\"Mobile Crushers\" \/><\/a>");
+document.write("                <div class=\"product-text\">");
+document.write("                    <h3>");
+document.write("                        <a rel=\"nofollow\" href=\"\/products\/mobile-crushers.html\">Mobile Crushers<\/a>");
+document.write("                    <\/h3>");
+document.write("                    <p>Mobile crusher is the machine that has crusher, feeder, feeding hopper, belt, driving and controlling systems on mobile chassis.<\/p>");
+document.write("                    <a rel=\"nofollow\" href=\"\/products\/mobile-crushers.html\" class=\"product-more\">Read More<\/a>");
+document.write("                <\/div>");
+document.write("            <\/li>");
+document.write("        <\/ul>");
+document.write("    <\/div>");
+document.write("  <div class=\"contact-form container mt60\" id=\"liuyan\">");
+document.write("        <strong>Drop us Message for any Query<\/strong>");
+
+document.write("     <form class=\"send\" id=\"Inquiry\" name=\"Inquiry\" method=\"post\" action=\"https:\/\/q.kssbchina.com\/information\/\" onsubmit=\"return doSubmits()\">");
+document.write("                <div class=\"input-panel\">");
+document.write("                <div class=\"me one\">");
+document.write("                    <input id=\"productname\" name=\"productname\" class=\"txt icon-text-pro\" placeholder=\"Product Name\" type=\"text\">");
+document.write("                <\/div>");
+document.write("                <div class=\"me two\">");
+document.write("                    <input id=\"name\" name=\"name\" class=\"txt icon-text-user\" placeholder=\"Your Name\" type=\"text\">");
+document.write("                <\/div>");
+document.write("                <div class=\"me three\">");
+document.write("                    <input id=\"email\" name=\"email\" class=\"txt icon-text-email\" placeholder=\"Your email(*)\" type=\"text\">");
+document.write("                <\/div>");
+document.write("                <div class=\"me four\">");
+document.write("                    <input id=\"telephone\" name=\"telephone\" class=\"txt icon-text-tel\" placeholder=\"Your Phone or Whatsapp\" type=\"text\">");
+document.write("                <\/div>");
+document.write("                <\/div>");
+document.write("                <div class=\"me five\">");
+document.write("                    <textarea id=\"message\" class=\"textarea icon-text-message\" name=\"message\" placeholder=\"Equipment name, model, related issues(*)\"><\/textarea>");
+document.write("                <\/div>");
+document.write("                <input id=\"site_feedback\" type=\"hidden\" name=\"site_feedback\">");
+document.write("                <input id=\"from_way\" type=\"hidden\" name=\"from_way\">");
+document.write("                <input id=\"refer\" type=\"hidden\" name=\"refer\">");
+document.write("                <div class=\"clearfix\"> <\/div>");
+document.write("                <div class=\"PorConSixXX\"><p><input type=\"checkbox\" class=\"choose_input\"><label><\/label><\/p>I accept the Data Protection Declaration<\/div>");
+document.write("                <input id=\"button_sub\" name=\"button_sub\" class=\"btn-primary\" type=\"submit\" value=\"Send Now!\">");
+document.write("            <\/form>");
+document.write("    <\/div>");
+
+
+function doSubmits() {
+    var stremail = document.getElementById("email");
+    if (stremail.value == "") {
+        alert('Please write down your correct Email');
+        stremail.focus();
+        return false;
+    } else {
+        var reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+        isok = reg.test(stremail.value);
+        if (!isok) {
+            alert("The format of the e-mail is incorrect, please re-enter the e-mail！");
+            stremail.focus();
+            return false;
+        }
+    }
+
+    var smessage = document.getElementById("message");
+    if (smessage.value == "") {
+        smessage.style.backgroundColor = "#bbc0c6";
+        alert('Please fill in the message');
+        smessage.focus();
+        return false;
+    }
+    var phone = document.getElementById("telephone");
+    if (phone.value !== "") {
+        var regs = /^[0-9]*$/;
+        isok = regs.test(phone.value);
+        leng = phone.value.length;
+        if (!isok || leng>20) {
+            alert("Please fill in the correct mobile number or Whatsapp;");
+            phone.focus();
+            return false;
+        }
+    }
+    // 1.客户端浏览器时间
+    var myDate = new Date();
+    var datenow = myDate.toLocaleDateString() + "-" + myDate.getHours() + ":" + myDate.getMinutes() + ":" + myDate.getSeconds();
+    //2. 随即字符串
+    var str = '';
+    var length = 10;
+    for (; str.length < length; str += Math.random().toString(36).substr(2));
+    str = str.substr(0, length);
+
+    //3.移动版
+    var mobileAgent = new Array("iphone", "ipod", "ipad", "android", "mobile", "blackberry", "webos", "incognito",
+        "webmate", "bada", "nokia", "lg", "ucweb", "skyfire");
+    var browser = navigator.userAgent.toLowerCase();
+    var isMobile = "pc";
+    for (var i = 0; i < mobileAgent.length; i++) {
+        if (browser.indexOf(mobileAgent[i]) != -1) {
+            isMobile = "m";
+            break;
+        }
+    }
+
+    window.document.Inquiry.site_feedback.value = str;
+    window.document.Inquiry.from_way.value = "|" + isMobile + "|";
+    window.document.Inquiry.refer.value = window.location.href + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    window.document.Inquiry.action = window.document.Inquiry.action + "?data=" + datenow + "&rnd=" + str + "&sys=" +
+        isMobile;
+    document.getElementById("button_sub").disabled = true;
+    window.document.Inquiry.submit();
+
+}
+
+
